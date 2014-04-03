6df38caa1c07cab79acad0d9bc0e0d4b94cda433
jillytot <jill@aylogames.com>
2014-04-02 21:32:41 -0700

    Terrain and bug behavior

---
+++ Assets/scripts/bughavior.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bughavior : MonoBehaviour {
+
+	public float speed = 1;
+	bool runAway = false;
+
+	// Use this for initialization
+	void Start () {
+
+		runAway = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (runAway == true) {
+
+			transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+		var runForIt = other.gameObject.CompareTag("hunter");
+
+		if (runForIt) {
+
+			Debug.Log("Run AWAY!!!");
+			runAway = true; 
+
+		} else { runAway = false;
+
+		}
+	}
+}
